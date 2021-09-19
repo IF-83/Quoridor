ba142a3233257b5930a5d2f7e756605d3240a0ab
IF <illesf16@gmail.com>
2021-09-19 11:52:59 +0200

    Spring application initialized.

---
+++ src/test/java/com/codecool/quoridor/QuoridorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.quoridor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoridorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
