faa198028749991da5e92e0246ad9d614d4e0a6d
IF <illesf16@gmail.com>
2021-10-31 10:43:14 +0100

    Nodelist and constructor added to graph class. For now it creates an empty graph.

--- src/main/java/com/codecool/websocket/service/BoardGraph.java
+++ src/main/java/com/codecool/websocket/service/BoardGraph.java
@@ -1,5 +1,15 @@
 package com.codecool.websocket.service;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BoardGraph {
-    
+
+    List<FieldNode> nodes;
+    public BoardGraph(GameLogic gameLogic) {
+        int boardSize = 81; //9 rows X 9 columns
+        for (int i = 0; i < boardSize; i++) {
+            nodes.add(new FieldNode());
+        }
+    }
+
 }
