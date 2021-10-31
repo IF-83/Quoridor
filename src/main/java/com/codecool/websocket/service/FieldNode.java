0c628fd1777c9794f121c4b8a28938238be47d36
IF <illesf16@gmail.com>
2021-10-31 10:08:02 +0100

    Node class 'FieldNode' implemented.

--- src/main/java/com/codecool/websocket/service/FieldNode.java
+++ src/main/java/com/codecool/websocket/service/FieldNode.java
@@ -1,5 +1,12 @@
 package com.codecool.websocket.service;
+import lombok.Data;
 
+@Data
 public class FieldNode {
+
+    private FieldNode left = null;
+    private FieldNode right = null;
+    private FieldNode up = null;
+    private FieldNode down = null;
 
 }
